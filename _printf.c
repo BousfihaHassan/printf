f2d0ed8a8a990537da79e320d382c415c3d02eb3
MrBoodj011 <adjarr20@gmail.com>
2023-11-13 13:42:37 -0800

    MrBoodj011

--- _printf.c
+++ _printf.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h> // Added for sprintf
+
 /**
  * _printf - Custom printf function.
  * @format: Format string with specifiers (%c, %s, %d/%i).
@@ -7,39 +11,60 @@
  */
 int _printf(const char *format, ...)
 {
-	int printed_chars = 0;
-	va_list arg_list;
+    int printed_chars = 0;
+    va_list arg_list;
 
-	if (format == NULL)
-		return (-1);
-	va_start(arg_list, format);
-	while (*format)
-	{
-		if (*format != '%')
-			write(1, format++, 1);
-		else
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == '%')
-				write(1, format++, 1);
-			else if (*format == 'c')
-				printed_chars++;
-				write(1, &va_arg(arg_list, int), 1);
-			else if (*format == 's')
-				char *str = va_arg(arg_list, char*)
-					;
-				for (; *str; str++, printed_chars++)
-					write(1, str, 1);
-			else if (*format == 'd' || *format == 'i')
-				char num_str[12];
-				int num_len = sprintf(num_str, "%d", va_arg(arg_list, int));
+    if (format == NULL)
+        return -1;
+    
+    va_start(arg_list, format);
 
-				printed_chars  += num_len;
-				write(1, num_str, num_len);
-		format++;
-	}
-	va_end(arg_list);
-	return (printed_chars);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            printed_chars++;
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+                break;
+            if (*format == '%')
+            {
+                write(1, "%", 1);
+                printed_chars++;
+            }
+            else if (*format == 'c')
+            {
+                char c = (char)va_arg(arg_list, int);
+                write(1, &c, 1);
+                printed_chars++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(arg_list, char*);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    printed_chars++;
+                }
+            }
+            else if (*format == 'd' || *format == 'i')
+            {
+                char num_str[12];
+                int num_len = sprintf(num_str, "%d", va_arg(arg_list, int));
+                write(1, num_str, num_len);
+                printed_chars += num_len;
+            }
+        }
+        format++;
+    }
+
+    va_end(arg_list);
+    return printed_chars;
 }
-
