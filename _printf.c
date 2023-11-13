30ff78580108ac5f3b5d91fab709051b5f185749
BousfihaHassan <bousfihahassan2019@gmail.com>
2023-11-13 11:58:10 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -2,62 +2,46 @@
 
 int _printf(const char *format, ...)
 {
-	int printed_chars = 0;
-	va_list arg_list;
+    int printed_chars = 0;
+    va_list arg_list;
 
-	if (format == NULL)
-	{
-		return (-1);
-	}
-	va_start(arg_list, format);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			printed_chars++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-			{
-				break;
-			}
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				printed_chars++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(arg_list, int);
-				write(1, &c, 1);
-				printed_chars++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(arg_list, char *);
-				int str_len = 0;
-				while (str[str_len] != '\0')
-				{
-					str_len++;
-				}
-				write(1, str, str_len);
-				printed_chars += str_len;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(arg_list, int);
-				char num_str[12];
-				int num_len = sprintf(num_str, "%d", num);
-				write(1, num_str, num_len);
-				printed_chars += num_len;
-			}
-		}
-		format++;
-	}
-	va_end(arg_list);
+    if (format == NULL)
+        return -1;
 
-	return printed_chars;
+    va_start(arg_list, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+            write(1, format++, 1);
+        else
+        {
+            format++;
+            if (*format == '\0')
+                break;
+            if (*format == '%')
+                write(1, format++, 1);
+            else if (*format == 'c')
+                printed_chars++, write(1, &va_arg(arg_list, int), 1);
+            else if (*format == 's')
+            {
+                char *str = va_arg(arg_list, char *);
+                for (; *str; str++, printed_chars++)
+                    write(1, str, 1);
+            }
+            else if (*format == 'd' || *format == 'i')
+            {
+                char num_str[12];
+                int num_len = sprintf(num_str, "%d", va_arg(arg_list, int));
+                printed_chars += num_len;
+                write(1, num_str, num_len);
+            }
+        }
+        format++;
+    }
+
+    va_end(arg_list);
+
+    return printed_chars;
 }
+
